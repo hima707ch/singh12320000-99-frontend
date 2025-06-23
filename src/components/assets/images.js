48d13fd06b679981d5f8f7907b1d4ca0cfc61915
root <root@aeed8319ead4>
2025-06-23 18:22:31 +0000

    Frontend setup and components update

---
+++ src/components/assets/images.js
@@ -0,0 +1,16 @@
+ const images = [
+  "https://realestate.apleona.com/fileadmin/media/images/ESG_Bild.png",
+    "https://prod.rockmedialibrary.com/api/public/content/ff061825fa8e44bf8108de5c786c0062?v=4c4f7c7a",
+    "https://psgroup.in/blog/wp-content/uploads/2020/11/banner-1.jpg",
+    "https://t4.ftcdn.net/jpg/02/85/83/41/360_F_285834130_nM5F6xl8lh8Mhb00ksNXBS6iBH9bMExz.jpg",
+    "https://images.ctfassets.net/n2ifzifcqscw/3QRMlAcJFrYAEAbhziixZW/d4b9aa50215c5ea7a161b8a6b59f1974/hero-real-estate-facts-trends.jpeg",
+    "https://www.cio.com/wp-content/uploads/2023/07/shutterstock_676661263.jpg?quality=50&strip=all&w=1024",
+    "https://img.etimg.com/thumb/width-420,height-315,imgsize-43988,resizemode-75,msid-107023656/magazines/panache/71-of-indias-wealthiest-are-keen-to-invest-in-luxury-real-estate-report/real-estate.jpg",
+    "https://static.vecteezy.com/system/resources/thumbnails/022/903/419/small/ai-generative-3d-modern-luxury-real-estate-house-for-sale-and-rent-luxury-property-concept-ai-generated-artwork-photo.jpg",
+    "https://cdn.prod.website-files.com/620ec747459e13c7cf12a39e/625b10a58137b364b18df2ea_iStock-94179607.jpg",
+    "https://media.gettyimages.com/id/1667208187/photo/male-real-estate-agent-talking-with-couple.jpg?s=612x612&w=gi&k=20&c=7IuHNGyQ1_CdLZUyGg0LQbdIA8ciDVRyzVnMratILOQ=",
+    "https://cdn.corporatefinanceinstitute.com/assets/real-estate.jpeg",
+    "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  ] 
+  
+  export default images;
